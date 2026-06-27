0faf27c0d0b1bbfb91c4f4b33ab614e40b0550b5
Trần Văn Nguyên <149607846+Tran-Van-Nguyen-05@users.noreply.github.com>
2026-06-27 23:18:34 +0700

    Update nf_geo.js

--- nf_geo.js
+++ nf_geo.js
@@ -1,21 +1,32 @@
-// nf_geo.js - Gia lap vi tri My de mo khoa catalog US
-let b = JSON.parse($response.body);
+// nf_geo.js - Spoof US Geolocation
+let b;
+try {
+    b = JSON.parse($response.body);
+} catch(e) {
+    b = {};
+}
+
 b = {
-  "status": "success",
-  "country": "US",
-  "countryCode": "US",
-  "region": "US",
-  "continent": "NA",
-  "ip": "157.240.0.0",
-  "proxyDetected": false,
-  "vpnDetected": false,
-  "dnsDetected": false,
-  "bypassGeo": true,
-  "globalAccess": true,
-  "catalogRegion": "US",
-  "allRegions": [
-    "US", "GB", "CA", "AU", "JP", "KR", "VN", "SG",
-    "DE", "FR", "IT", "ES", "NL", "BR", "MX", "IN"
-  ]
+    "status": "success",
+    "country": "US",
+    "countryCode": "US",
+    "region": "US",
+    "continent": "NA",
+    "ip": "157.240.0.1",
+    "proxyDetected": false,
+    "vpnDetected": false,
+    "dnsDetected": false,
+    "torDetected": false,
+    "relayDetected": false,
+    "hostingDetected": false,
+    "bypassGeo": true,
+    "globalAccess": true,
+    "catalogRegion": "US",
+    "preferredCatalog": "US",
+    "allRegions": [
+        "US", "GB", "CA", "AU", "JP", "KR", "VN", "SG",
+        "DE", "FR", "IT", "ES", "NL", "BR", "MX", "IN"
+    ]
 };
+
 $done({body: JSON.stringify(b)});
